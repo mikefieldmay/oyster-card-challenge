4f7070dd9183ba7938009e7680c3fbc1650da67e
Rick Clark <rkingsleyclark@gmail.com>
2017-01-09 16:42:38 +0000

    Created pryrc and top up limit unit test

---
+++ .pryrc
@@ -0,0 +1,3 @@
+require_relative "lib/oystercard.rb"
+
+oystercard = Oystercard.new
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,24 +6,19 @@ describe Oystercard do
 
   describe ".balance" do
 
-    it { is_expected.to respond_to(:balance) }
-
     context "default balance" do
       it {expect(oystercard.balance).to eq 0}
     end
 
-    context "specify balance" do
+    context "specify balance of 30" do
       subject(:oystercard) { described_class.new(30) }
 
       it { expect(oystercard.balance).to eq 30 }
 
     end
-
   end
 
   describe ".top_up" do
-
-    it { is_expected.to respond_to(:top_up).with(1).argument }
 
     context "topping up card" do
 
@@ -33,6 +28,15 @@ describe Oystercard do
       end
 
     end
+
+    context "top up limit" do
+      it "has a limit of 90" do
+        expect { oystercard.top_up(91) }.to raise_error(RuntimeError, "Cannot top up above £90")
+      end
+
+
+    end
+
   end
 
 end
