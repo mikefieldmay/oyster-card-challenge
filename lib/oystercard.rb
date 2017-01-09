3bcb62f6de0e557d6b2119b33a45aa3300119148
Rick Clark <rkingsleyclark@gmail.com>
2017-01-09 16:21:49 +0000

    Added top_up method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -7,4 +7,8 @@ class Oystercard
     @balance = balance
   end
 
+  def top_up(amount)
+
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,19 +6,24 @@ describe Oystercard do
 
   describe ".balance" do
 
-    it {expect(oystercard).to respond_to(:balance)}
+    it { is_expected.to respond_to(:balance) }
 
     context "default balance" do
       it {expect(oystercard.balance).to eq 0}
     end
 
     context "specify balance" do
-      subject(:oystercard) { described_class.new(30)}
+      subject(:oystercard) { described_class.new(30) }
 
       it { expect(oystercard.balance).to eq 30 }
 
     end
 
+  end
+
+  describe ".top_up" do
+
+    it { is_expected.to respond_to(:top_up).with(1).argument }
   end
 
 end
