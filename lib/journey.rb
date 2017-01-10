2536a49af4168b75cf95ff1d4e945822a8162347
Lauren Rosie <laurenrosiewatson@gmail.com>
2017-01-10 18:05:17 +0000

    Added methods #finish and #complete? methods to Journey

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,9 +1,19 @@
 class Journey
 
-  attr_reader :entry_station
+  attr_reader :entry_station, :exit_station
+
 
   def start(entry_station)
     @entry_station = entry_station
+    @complete = false
   end
 
+  def finish(exit_station)
+    @exit_station = exit_station
+    @complete = true
+  end
+
+  def complete?
+    @complete
+  end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -5,9 +5,10 @@ describe Journey do
   subject(:journey) { described_class.new }
 
   let(:station) {instance_double(Station)}
+  let(:another_station) {instance_double(Station)}
+
 
   describe "#start" do
-    it { is_expected.to respond_to(:start) }
 
     context "when starting journey" do
       it "should take station as an argument" do
@@ -18,9 +19,25 @@ describe Journey do
         expect(journey.entry_station).to eq station
       end
     end
-
-
   end
 
+describe "#finish" do
+
+  context "when finishing a journey" do
+    it "sets the exit station attribute " do
+      journey.finish(another_station)
+      expect(journey.exit_station).to eq another_station
+    end
+  end
+end
+
+describe "#complete?" do
+  context "whilst in a journey" do
+  it "returns a boolean" do
+    journey.start(station)
+    expect(journey.complete?).to eq false
+  end
+end
+end
 
 end
